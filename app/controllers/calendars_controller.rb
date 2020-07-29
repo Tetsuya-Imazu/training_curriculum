c7738b45e78418f6bc0794be372eb821c67ecee8
Tetsuya-Imazu <tty8131b@gmail.com>
2020-07-29 15:42:52 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -14,8 +14,9 @@ class CalendarsController < ApplicationController
 
   private
 
+
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
